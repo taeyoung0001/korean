20aa2718022e0039228afcb3b5fe24c0cddd2d8d
taeyoung <her020212@naver.com>
2025-03-05 21:24:37 +0900

    [Feat]detail페이지 생성

---
+++ src/app/components/detailNavBar.tsx
@@ -0,0 +1,20 @@
+import HomeSearch from "./homeSearch";
+import styles from "@/app/styles/detail/detailNavBar.module.scss";
+import cn from "classnames/bind";
+
+const cx = cn.bind(styles);
+
+/* eslint-disable @next/next/no-img-element */
+const DetailNavBar = () => {
+  return (
+    <div className={cx("navbar-wrap")}>
+      <div className={cx("img-wrap")}>
+        <img src="/detailLogo.png" alt="" />
+      </div>
+
+      <HomeSearch />
+    </div>
+  );
+};
+
+export default DetailNavBar;
---
+++ src/app/view/Detail/DetailView.tsx
@@ -0,0 +1,11 @@
+import DetailNavBar from "@/app/components/detailNavBar";
+
+const DetailView = () => {
+  return (
+    <div>
+      <DetailNavBar />
+    </div>
+  );
+};
+
+export default DetailView;
