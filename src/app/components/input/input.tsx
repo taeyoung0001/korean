e3e33a671e62bf5a66a3bd6926e7a0e9d354b571
taeyoung <her020212@naver.com>
2025-03-05 15:58:11 +0900

    [Feat] input 컴포넌트 생성, css 생성

--- src/app/components/input/input.tsx
+++ src/app/components/input/input.tsx
@@ -1,5 +1,26 @@
-const Input = () => {
-  return <div>Enter</div>;
+import React from "react";
+import styles from "./input.module.scss";
+import cn from "classnames/bind";
+
+const cx = cn.bind(styles);
+
+type InputProps = {
+  onChange?: () => void;
+  placeHolder?: string;
+  className: "large" | "medium" | "small";
+};
+const Input = (props: InputProps) => {
+  const { onChange, placeHolder, className = "medium" } = props;
+  return (
+    <div className={cx("input-container")}>
+      <input
+        className={cx("input", className)}
+        onChange={onChange}
+        placeholder={placeHolder}
+        type="text"
+      />
+    </div>
+  );
 };
 
 export default Input;
