948ff87a3f1d3f21cd781ccb7029b60a912b7f3d
taeyoung <her020212@naver.com>
2025-03-08 09:51:33 +0900

    [Feat] noward페이지 완성

--- src/app/components/noneWord.tsx
+++ src/app/components/noneWord.tsx
@@ -1,14 +1,30 @@
+/* eslint-disable react/no-unescaped-entities */
 import styles from "@/app/styles/components/noneWord.module.scss";
 import cn from "classnames/bind";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const cx = cn.bind(styles);
 
 const NoneWord = () => {
+  const pathname = usePathname();
+  const slug = pathname?.split("/detail/")[1];
+  const decodedSlug = slug ? decodeURIComponent(slug) : "값 없음";
+
+  if (!slug) {
+    return <p>loading</p>;
+  }
   return (
-    <div className={cx("container")}>
-      <div>죄송합니다... 검색하신 단어는 사전에 없는 단어 입니다.</div>
-      <div>
-        <button>처음 화면으로 이동하기</button>
+    <div className={cx("wrap")}>
+      <div className={cx("word-wrap")}>
+        <p>
+          죄송합니다...검색하신 단어 "{decodedSlug}"는 사전에 없는 단어 입니다.
+        </p>
+        <div className={cx("button-container")}>
+          <Link href={"/"}>
+            <button>처음 화면으로 이동하기</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
